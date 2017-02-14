868dd9de712e6669a6201877f5ebc167e5eaff0e
Martin Chovanec <chovamar@fit.cvut.cz>
2017-02-14 16:06:49 +0100

    Optimise loop to use Truffle's LoopNode

--- pascal/src/main/java/cz/chovanecm/pascal/truffle/nodes/controlflow/WhileNode.java
+++ pascal/src/main/java/cz/chovanecm/pascal/truffle/nodes/controlflow/WhileNode.java
@@ -1,11 +1,11 @@
 package cz.chovanecm.pascal.truffle.nodes.controlflow;
 
-import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.dsl.UnsupportedSpecializationException;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.LoopNode;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
-import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import com.oracle.truffle.api.nodes.RepeatingNode;
 import cz.chovanecm.pascal.truffle.PascalTypesGen;
 import cz.chovanecm.pascal.truffle.nodes.ExpressionNode;
 import cz.chovanecm.pascal.truffle.nodes.StatementNode;
@@ -21,24 +21,37 @@ import cz.chovanecm.pascal.truffle.nodes.StatementNode;
 @NodeInfo(shortName = "while")
 public class WhileNode extends StatementNode {
     @Child
-    private ExpressionNode conditionNode;
-    @Child
-    private StatementNode loopNode;
+    private LoopNode loop;
 
     public WhileNode(ExpressionNode conditionNode, StatementNode loopNode) {
-        this.conditionNode = conditionNode;
-        this.loopNode = loopNode;
+
+        loop = Truffle.getRuntime().createLoopNode(new WhileRepeatingNode(conditionNode, loopNode));
     }
 
     @Override
     public void execute(VirtualFrame frame) {
-        try {
-            while (PascalTypesGen.expectBoolean(conditionNode.execute(frame))) {
+        loop.executeLoop(frame);
+    }
+
+    public static class WhileRepeatingNode extends Node implements RepeatingNode {
+        @Child
+        private ExpressionNode conditionNode;
+        @Child
+        private StatementNode loopNode;
+
+        public WhileRepeatingNode(ExpressionNode conditionNode, StatementNode loopNode) {
+            this.conditionNode = conditionNode;
+            this.loopNode = loopNode;
+        }
+
+        @Override
+        public boolean executeRepeating(VirtualFrame frame) {
+            if (PascalTypesGen.asBoolean(conditionNode.execute(frame))) {
                 loopNode.execute(frame);
+                return true;
+            } else {
+                return false;
             }
-        } catch (UnexpectedResultException e) {
-            CompilerDirectives.transferToInterpreter();
-            throw new UnsupportedSpecializationException(this, new Node[]{conditionNode}, e.getResult());
         }
     }
 }
