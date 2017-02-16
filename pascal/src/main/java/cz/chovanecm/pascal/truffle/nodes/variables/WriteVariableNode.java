5653d23ac2241f23c7774d86b7cd85600d3a47cd
Martin Chovanec <chovanecm@gmail.com>
2017-02-16 13:33:34 +0100

    Wrong way... collatz took 11 s

--- pascal/src/main/java/cz/chovanecm/pascal/truffle/nodes/variables/WriteVariableNode.java
+++ pascal/src/main/java/cz/chovanecm/pascal/truffle/nodes/variables/WriteVariableNode.java
@@ -1,10 +1,12 @@
 package cz.chovanecm.pascal.truffle.nodes.variables;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.*;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotKind;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.api.profiles.ConditionProfile;
 import cz.chovanecm.pascal.exceptions.TypeException;
 import cz.chovanecm.pascal.truffle.nodes.ExpressionNode;
 import cz.chovanecm.pascal.truffle.nodes.StatementNode;
@@ -21,12 +23,13 @@ public abstract class WriteVariableNode extends StatementNode {
     public abstract FrameSlot getFrameSlot();
 
     public abstract String getVariableName();
+    private ConditionProfile typeCheckProfile = ConditionProfile.createBinaryProfile();
 
     @Specialization
     public void writeLong(VirtualFrame frame, long value) throws TypeException {
-        //Slow! Better to already know the slot!
         FrameSlot slot = getFrameSlot();
-        if (slot.getKind() != FrameSlotKind.Long) {
+        if (typeCheckProfile.profile(slot.getKind() != FrameSlotKind.Long)) {
+            //CompilerDirectives.transferToInterpreter();
             throw new TypeException("Trying to write a long value to variable " + getVariableName());
         }
         frame.setLong(slot, value);
@@ -35,7 +38,8 @@ public abstract class WriteVariableNode extends StatementNode {
     @Specialization
     public void writeBoolean(VirtualFrame frame, boolean value) throws TypeException {
         FrameSlot slot = getFrameSlot();
-        if (slot.getKind() != FrameSlotKind.Boolean) {
+        if (typeCheckProfile.profile(slot.getKind() != FrameSlotKind.Boolean)) {
+            //CompilerDirectives.transferToInterpreter();
             throw new TypeException("Trying to write a boolean value to variable " + getVariableName());
         }
         frame.setBoolean(slot, value);
@@ -44,7 +48,8 @@ public abstract class WriteVariableNode extends StatementNode {
     @Specialization
     public void writeDouble(VirtualFrame frame, double value) throws TypeException {
         FrameSlot slot = getFrameSlot();
-        if (slot.getKind() != FrameSlotKind.Double) {
+        if (typeCheckProfile.profile(slot.getKind() != FrameSlotKind.Double)) {
+            //CompilerDirectives.transferToInterpreter();
             throw new TypeException("Trying to write a double value to variable " + getVariableName());
         }
         frame.setDouble(slot, value);
@@ -53,7 +58,8 @@ public abstract class WriteVariableNode extends StatementNode {
     @Specialization
     public void writeString(VirtualFrame frame, String value) throws TypeException {
         FrameSlot slot = getFrameSlot();
-        if (slot.getKind() != FrameSlotKind.Object) {
+        if (typeCheckProfile.profile(slot.getKind() != FrameSlotKind.Object)) {
+            //CompilerDirectives.transferToInterpreter();
             throw new TypeException("Trying to write a String value to variable " + getVariableName());
         }
         frame.setObject(slot, value);
